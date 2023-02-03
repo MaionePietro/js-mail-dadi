669d67f0e6140b6459277c76e234ffe78f78e4ac
MaionePietro <pietromaionespam@gmail.com>
2023-02-03 14:46:21 +0100

    FEAT innerHTML for email

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-
-
 let scelta = prompt("Premi 1 per le email, 2 per i dadi");
 if(scelta == 1){
     //email
@@ -11,20 +9,22 @@ if(scelta == 1){
         const emailInput = prompt("Inserisci email");
         arreyEmail.push(emailInput);
         console.log('email inserita: '+arreyEmail[i]);
+        const printEmail = document.querySelector('body')
+        printEmail.innerHTML += ('<h1>Email: '+arreyEmail[i]+'</h1>');
     }
 
 }else if(scelta == 2){
     for(let i=0 ; i<=1 ; i++){
         let utenteRandom = Math.floor(Math.random()*6);
-        console.log(utenteRandom);
+        console.log('numero utente:'+utenteRandom);
         let pcRandom = Math.floor(Math.random()*6);
-        console.log(pcRandom);
+        console.log('numero pc:'+pcRandom);
         if(utenteRandom==pcRandom){
-            alert('pari')
+            alert('PARI')
         }else if(utenteRandom>pcRandom){
             alert('HAI VINTO')
         }else{
-            alert('PERSO')
+            alert('HAI PERSO')
         }
         i = prompt("Premi 0 per riprovare o 1 per chiudere");
     }
