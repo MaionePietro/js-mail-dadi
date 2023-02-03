8ca050c46ece54c82602821faba9b7db039797f5
MaionePietro <pietromaionespam@gmail.com>
2023-02-03 13:29:37 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+let i=0, numeroEmail=0;
+let arreyEmail = [];
+numeroEmail = prompt("Quante email devi inserire?");
+console.log(numeroEmail);
+for(i ; i< numeroEmail ; i++){
+    const emailInput = prompt("Inserisci email")
+    arreyEmail.push(emailInput);
+    console.log(arreyEmail);
+}
